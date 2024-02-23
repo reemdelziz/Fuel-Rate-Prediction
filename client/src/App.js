da04ae750bab64b18f675790db45a5c19ec741a7
theDevin8 <devinstockton2003@gmail.com>
2024-02-22 22:39:32 -0600

    starting on version2

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,8 @@
 import './App.css';
-import { Canvas } from '@react-three/fiber';
-import { Experience } from './components/Experience';
-import { ScrollControls } from '@react-three/drei';
 
 function App() {
   return (
     <>
-      <Canvas >
-        <color attach="background" args={["#ececec"]} />
-        <ScrollControls pages= {10} damping={1}>
-          <Experience />
-        </ScrollControls>
-      </Canvas>
       
     </>
   );
