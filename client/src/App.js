4379b64a3264d3c8ef6936e5d604345744e23686
migueleog01 <99515060+migueleog01@users.noreply.github.com>
2024-02-23 18:35:20 -0600

    added FuelQuote import

--- client/src/App.js
+++ client/src/App.js
@@ -2,11 +2,15 @@ import './App.css';
 import { FuelHistory } from './pages/FuelHistory';
 import { Nav } from './components/Nav';
 import { Profile } from './pages/Profile';
+import { FuelQuote } from './pages/FuelQuote';
+
 
 function App() {
   return (
     <>
       <Nav />
+      {/*<Profile/>*/}
+      {/*<FuelQuote />*/}
       <FuelHistory />
     </>
   );
