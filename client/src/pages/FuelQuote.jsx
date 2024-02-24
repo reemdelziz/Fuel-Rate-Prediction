938599fc846d9702243862f8c2f207bcf52edb9c
migueleog01 <99515060+migueleog01@users.noreply.github.com>
2024-02-23 18:26:52 -0600

    Added fuelquote page

---
+++ client/src/pages/FuelQuote.jsx
@@ -0,0 +1,71 @@
+import React  from 'react';
+import Nav from '../components/Nav';
+
+
+//create funtion print
+function FuelQuote() {
+    return (
+        <div>
+            {/*include nav bar*/}
+            <Nav />
+
+
+            <div className = "PageTitle" >Fuel Quote.</div>
+            
+
+            <div class="container">
+                <form className="form-group">
+                    <div className="form-row">
+                    <div className="form-field">
+                        <label htmlFor="GallonsRequested">Gallons Requested*</label>
+                        <input type="text" id="GallonsRequested" className="input-field" />
+                    </div>
+                    <div className="form-field">
+                        <label htmlFor="DeliveryAddress">Delivery Address*</label>
+                        <input type="text" id="DeliveryAddress" className="input-field" />
+                    </div>
+                    </div>
+
+                    <div className="form-row">
+                    <div className="form-field">
+                        <label htmlFor="DeliveryDate">Delivery Date*</label>
+                        <input type="text" id="DeliveryDate" className="input-field" />
+                    </div>
+                    <div className="form-field">
+                        <input type="submit" value="Submit" className="submit-button" />
+                    </div>
+                    </div>
+                </form>
+
+                <div className='profileMessage'>
+                    When requesting a fuel quote, each estimate is carefully calculated to offer you the best possible price based on current market conditions and your specific requirements. Our aim is to ensure that you receive a competitive and fair quote, helping you manage your fuel expenses efficiently and effectively.
+                </div>
+                </div>
+
+                <div class="price-amount-labels">
+                    <div class="SuggestedPrice-label">
+                        <span>Suggested Price / Gallon:</span>
+                    </div>
+                    <div class="TotalAmount-label">
+                        <span>Total Amount Due:</span>
+                    </div>
+                    </div>
+
+                    <div class="price-amount-values">
+                    <div class="SuggestedPrice-value">
+                        <span id="suggestedPriceValue">$0.00</span> 
+                    </div>
+                    <div class="TotalAmount-value">
+                        <span id="totalAmountDueValue">$0.00</span> 
+                    </div>
+
+                </div>
+
+            </div>
+  
+       
+    );
+
+
+}
+export default FuelQuote;
